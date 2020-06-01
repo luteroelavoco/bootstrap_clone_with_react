b5420b5818e9d66ec5aaa0d932dd9937bef2ec2c
cientista1 <40244138+cientista1@users.noreply.github.com>
2020-06-01 02:39:36 -0300

    Updates

---
+++ precache-manifest.7442a9df2c528588a0521147e11001ff.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f9d96be8fb1f2108d21c30771380da9",
+    "url": "/bootstrap_clone_with_react/index.html"
+  },
+  {
+    "revision": "6104b63f1e10e82011f0",
+    "url": "/bootstrap_clone_with_react/static/css/main.88e639c3.chunk.css"
+  },
+  {
+    "revision": "bbdc6f35b365494fd734",
+    "url": "/bootstrap_clone_with_react/static/js/2.6aa0dcc4.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/bootstrap_clone_with_react/static/js/2.6aa0dcc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6104b63f1e10e82011f0",
+    "url": "/bootstrap_clone_with_react/static/js/main.bdf3173b.chunk.js"
+  },
+  {
+    "revision": "4c53bbcca1cc12d03f3c",
+    "url": "/bootstrap_clone_with_react/static/js/runtime-main.cf8d0f46.js"
+  },
+  {
+    "revision": "7384412324c769e447d3c4f86e9ff068",
+    "url": "/bootstrap_clone_with_react/static/media/bootstrap-stack.73844123.png"
+  },
+  {
+    "revision": "ebc6880a0d4ea3be8573ee9af6c80f15",
+    "url": "/bootstrap_clone_with_react/static/media/bootstrap-themes.ebc6880a.png"
+  },
+  {
+    "revision": "46a5cd73831212febd2a96a1253a226d",
+    "url": "/bootstrap_clone_with_react/static/media/logo.46a5cd73.JPG"
+  }
+]);
